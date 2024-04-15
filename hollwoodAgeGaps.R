64ce49315e51462c035ab3e9779413abe02dfb5b
glow-27 <94476661+glow-27@users.noreply.github.com>
2024-04-15 09:51:48 -0700

    Add files via upload

---
+++ hollwoodAgeGaps.R
@@ -0,0 +1,18 @@
+# Get the Data
+
+# Read in with tidytuesdayR package 
+# Install from CRAN via: install.packages("tidytuesdayR")
+# This loads the readme and all the datasets for the week of interest
+
+# Either ISO-8601 date or year/week works!
+
+tuesdata <- tidytuesdayR::tt_load('2023-02-14')
+tuesdata <- tidytuesdayR::tt_load(2023, week = 7)
+
+age_gaps <- tuesdata$age_gaps
+
+# Or read in the data manually
+
+age_gaps <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-14/age_gaps.csv')
+
+head(age_gaps)
